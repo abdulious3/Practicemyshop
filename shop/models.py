1b4ab7004bc1511dafabfc440b92bbad3efb62e4
abdulious3 <robinson82425@gmail.com>
2023-05-12 03:15:34 -0700

    updates trying to fix error

--- shop/models.py
+++ shop/models.py
@@ -3,14 +3,11 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200,
-                            unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
         ordering = ['name']
-        indexes = [
-            models.Index(fields=['name']),
-        ]
+        indexes = [models.Index(fields=['name']),]
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
@@ -19,16 +16,13 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category,
-                                 related_name='products',
-                                 on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
-    image = models.ImageField(upload_to='products/%Y/%m/%d',
-                              blank=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10,
-                                decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
